a1cf7749ca9ccd53267f2cf1e6411318d8699365
Julian De Ocampo <julian.deocampo@gmail.com>
2014-11-28 18:52:29 -0700

    updated problem 1

--- problem1.js
+++ problem1.js
@@ -4,8 +4,9 @@
 
 var sum = 0;
 
-for (var i=1; i<=1000; i++)
+for (var i=1; i<1000; i++)
 	if(i % 3 == 0 || i % 5 == 0)
-		sum = sum + i;
+		{sum = sum + i;
+		console.log(sum);}
 
 console.log(sum);
